efccdb0a3c80ffb1fd0574eddb1c93df53b4c93c
PopMain <popmain@github.com>
2020-12-05 23:38:43 +0800

    optimization

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,11 @@
+package sort;
+
+/**
+ * 快速排序
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -43,10 +43,12 @@ public class ShellSort {
         int gap = input.length / 2;
         while (gap > 0) {
             System.out.println("gap="+gap);
-            for (int realIndex = gap; realIndex < input.length;  realIndex += gap) {
-                for (int j = realIndex ; j > 0; j = j - gap) {
-                    if (input[j] < input[j - gap]) {
-                        Utils.swap(input, j, j - gap);
+            for (int i = 0; i < gap; i ++) {
+                for (int realIndex = i; realIndex < input.length; realIndex += gap) {
+                    for (int j = realIndex; j > i; j = j - gap) {
+                        if (input[j] < input[j - gap]) {
+                            Utils.swap(input, j, j - gap);
+                        }
                     }
                 }
             }
