1754791c92ce08fe830ec03d1e53764fea6ccb61
PopMain <popmain@github.com>
2020-12-05 23:34:21 +0800

    optimization

--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -43,7 +43,7 @@ public class ShellSort {
         int gap = input.length / 2;
         while (gap > 0) {
             System.out.println("gap="+gap);
-            for (int i = 0, realIndex = gap; realIndex < input.length; i++, realIndex += gap) {
+            for (int realIndex = gap; realIndex < input.length;  realIndex += gap) {
                 for (int j = realIndex ; j > 0; j = j - gap) {
                     if (input[j] < input[j - gap]) {
                         Utils.swap(input, j, j - gap);
