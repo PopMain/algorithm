35e503f5ab4b714f22837d8f73df7a0d371e032a
吴志欣 <wzx@meitu.com>
2021-04-23 16:42:15 +0800

    LongestPalindrome

---
+++ src/dynamic/LongestPalindrome.java
@@ -0,0 +1,75 @@
+package dynamic;
+
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 示例 3：
+ *
+ * 输入：s = "a"
+ * 输出："a"
+ * 示例 4：
+ *
+ * 输入：s = "ac"
+ * 输出："a"
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母（大写和/或小写）组成
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return  "";
+        }
+        if (s.length() == 1) {
+            return s;
+        }
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+        for (int j = 0; j < len; j ++) {
+            for (int i = j - 1; i >= 0; i--) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+}
